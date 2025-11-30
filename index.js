0be6bccfd0dd8b607263a2616e931d5e7d7f8140
Viktor-Yashchuk <143907417+Viktor-Yashchuk@users.noreply.github.com>
2025-11-30 13:47:25 +0000

    Deploying to gh-pages from @ Viktor-Yashchuk/products-Shop@3d331fb57cf5c987ae9b6888463603120e0019dd 🚀

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import"./assets/styles-JE8YjOlG.js";import{a as s,i as a}from"./assets/vendor-4yCzdkXl.js";const c="https://dummyjson.com/",r={CATEGORIES:"products/category-list"};s.defaults.baseURL=c;const i=async()=>{const{data:t}=await s(`${r.CATEGORIES}`);return t},n={categoryList:document.querySelector(".categories")},g=t=>{const e=t.map(o=>`<li class="categories__item">
+import"./assets/styles-JE8YjOlG.js";import{a as c,i as r}from"./assets/vendor-4yCzdkXl.js";const g="https://dummyjson.com/",e={CATEGORIES:"products/category-list",PRODUCTS:"products"};c.defaults.baseURL=g;const m=async()=>{const{data:t}=await c(`${e.CATEGORIES}`);return t},_=async t=>{const{data:s}=await c(`${e.PRODUCTS}`,{params:{limit:12,skip:(t-1)*12}});return s},a={categoryList:document.querySelector(".categories"),productList:document.querySelector(".products")},y=t=>{const s=t.map(o=>`<li class="categories__item">
    <button class="categories__btn" type="button">${o}</button>
- </li>`).join("");n.categoryList.innerHTML=e},l=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},m=async()=>{try{const t=await i();g(["All",...t]),l()}catch(t){console.log(t),a.error({message:"Oops, something went wrong!"})}};m();
+ </li>`).join("");a.categoryList.innerHTML=s},b=t=>{const s=t.map(({id:o,thumbnail:n,tags:i,title:p,brand:d,category:l,price:u})=>`<li class="products__item" data-id="${o}">
+    <img class="products__image" src="${n}" alt="${i}"/>
+    <p class="products__title">${p}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${d}</span></p>
+    <p class="products__category">Category: ${l}</p>
+    <p class="products__price">Price: ${u}$</p>
+ </li>
+`).join("");a.productList.insertAdjacentHTML("beforeend",s)},$=()=>{const t=document.querySelector(".categories__btn");t&&t.classList.add("categories__btn--active")},L=async()=>{try{const t=await m();y(["All",...t]),$()}catch(t){console.log(t),r.error({message:"Oops, something went wrong!"})}},S=async()=>{try{const{products:t}=await _(1);b(t)}catch(t){console.log(t),r.error({message:"Oops, something went wrong!"})}};L();S();
 //# sourceMappingURL=index.js.map
