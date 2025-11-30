1073d850faf17f5c11d7cc52605817acb8d85d67
Sasha <sasha21100@ukr.net>
2025-11-30 15:36:23 +0200

    products

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -11,3 +11,17 @@ export const renderCategories = data => {
 
   refs.categoryList.innerHTML = markup;
 };
+
+
+export const renderProducts = data => {
+  const markup = data
+  .map(({id, thumbnail, tags, title, brand, category, price})=> `<li class="products__item" data-id="${id}">
+    <img class="products__image" src="${thumbnail}" alt="${tags}"/>
+    <p class="products__title">${title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${brand}</span></p>
+    <p class="products__category">Category: ${category}</p>
+    <p class="products__price">Price: ${price}$</p>
+ </li>
+`).join('');
+refs.productList.insertAdjacentHTML('beforeend', markup)
+}
